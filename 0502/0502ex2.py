7daef6f15ab621dc72579f07d2da46948bc3adcf
김현탁 <86011707+janalai3395@users.noreply.github.com>
2023-05-02 13:23:42 +0900

    5월 2일 수업

---
+++ 0502/0502ex2.py
@@ -0,0 +1,16 @@
+'''
+2023-05-02
+김현탁
+range()함수-순차적인 범위의 숫자 생성하는 함수
+'''
+for i in range(10): #반복조건
+    print(i,end=' ') #반복조건이 참인동안 반복
+print()
+for i in range(1,10): #반복조건
+    print(i,end=' ')
+print()
+for j in range(1,12,2): #반복조건
+    print(j,end=' ') #홀수 출력
+print()
+for num in  range(10,0,-1): #반복조건
+    print(num,end=' ')
