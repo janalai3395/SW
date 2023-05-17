08f336376e80c1c6a8c70a7914ea9295c5b0bb48
김현탁 <86011707+janalai3395@users.noreply.github.com>
2023-05-17 15:22:23 +0900

    5/17 진행내용

---
+++ 0517/0517ex1.py
@@ -0,0 +1,37 @@
+'''
+2023-05-17
+김현탁
+for문을 이용한 파일 읽기
+'''
+
+# for 반복문으로 파일 읽어 오기
+with open("c:/data/linetest.txt","r") as f: #파일 열기
+    for line in f: #파일 객체를 지정해서 반복문 작성
+        print(line,end='') #줄 바꿈 없이 출력
+
+#readline() - (한 줄씩 읽어 오기) 메소드로 파일 읽어 오기
+print()
+
+with open("c:/data/linetest.txt","r") as f: #파일 열기
+    while True: #무한 반복
+        line=f.readline() #한줄씩 읽어오기
+        print(line,end='') #줄 바꿈 없이 출력
+        if line=='': #읽어 올 내용이 없다면
+            break
+
+#readlines(): 한 줄씩 읽어서 리스트형으로 반환
+print()
+
+with open("c:/data/linetest.txt","r") as f: #파일 열기
+    textlists=f.readlines()#한 줄씩 리스트 형식으로 읽어 오기
+    print(type(textlists)) #변수 타입 확인
+    print(textlists)
+
+#print()함수로 파일에 출력하기
+f=open("c:/data/ptest.txt","w") #쓰기 모드의 파일 객체 생성
+
+print("aaaaaa",file=f) #파일에 출력하기
+print("bbbbbb",file=f) #파일에 출력하기
+print("cccccc",file=f) #파일에 출력하기
+
+f.close() #파일 닫기
